b5cc3889cd3a16616b19d4d4705c07cd210922c4
Yusheng Ding <davidthinkleding@gmail.com>
2016-02-10 22:50:47 -0600

    1

--- mySite/mySite/settings.py
+++ mySite/mySite/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'mySite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mySite',
+        'USER': 'root',
+        'PASSWORD': 'password',
     }
 }
 
